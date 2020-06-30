da0c45920e389ce2bfb56e6f2c41a3d4273296f9
Kishor Sutar <kishor007sutar@gmail.com>
2020-06-30 22:27:20 +0100

    Two number sum problem

---
+++ src/main/kotlin/com/kishor/algo/Main.kt
@@ -0,0 +1,10 @@
+package com.kishor.algo
+
+import com.kishor.algo.problems.easy.twoNumberSumWithSort
+
+
+fun main(args: Array<String>) {
+    println("Hello world")
+    println(twoNumberSumWithSort(arrayOf(1,3,5,7,8,9), 4))
+}
+
