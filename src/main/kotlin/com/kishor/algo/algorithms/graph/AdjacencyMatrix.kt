5d80c3753eb778b83b19397d6fe84226df689995
Kishor Sutar <kishor007sutar@gmail.com>
2020-07-03 23:25:37 +0100

    DFS graph traversal and connected check

--- src/main/kotlin/com/kishor/algo/algorithms/graph/AdjacencyMatrix.kt
+++ src/main/kotlin/com/kishor/algo/algorithms/graph/AdjacencyMatrix.kt
@@ -5,13 +5,15 @@ import java.util.*
 fun main() {
     val adjM = AdjacencyMatrix(7)
     adjM.accept(Scanner(System.`in`))
-    adjM.display()
+//    adjM.display()
+    adjM.dfsTrav(0)
+    println("${adjM.dfsIsConnected(0)}")
 }
 
 class AdjacencyMatrix(var vertCount: Int = 0) {
 
     private var edgeCount: Int = 0
-    private var matrix = Array(vertCount) { Array(vertCount) {0} }
+    private var matrix = Array(vertCount) { Array(vertCount) { 0 } }
 
     fun accept(sc: Scanner) {
         println("Enter edge count : ")
@@ -29,12 +31,65 @@ class AdjacencyMatrix(var vertCount: Int = 0) {
     fun display() {
         for (i in 0 until vertCount) {
             println()
-            for(j in 0 until edgeCount) {
+            for (j in 0 until edgeCount) {
                 print("\t${matrix[i][j]} ")
             }
             println()
         }
     }
+
+    fun dfsTrav(start: Int) {
+        val stack = Stack<Int>()
+        val visited = BooleanArray(vertCount) { false }
+
+        stack.push(start)
+        visited[start] = true
+
+        while (stack.isNotEmpty()) {
+            val vert = stack.pop()
+
+            print("\t $vert")
+            for (i in 0 until vertCount) {
+                if (!visited[i]
+                    && matrix[vert][i] == 1
+                ) {
+                    stack.push(i)
+                    visited[i] = true
+                }
+            }
+        }
+        println()
+    }
+
+    fun dfsIsConnected(start: Int): Boolean {
+        val stack = Stack<Int>()
+        val visited = BooleanArray(vertCount) { false }
+        var count = 0
+        stack.push(start)
+        visited[start] = true
+        count++
+        while (stack.isNotEmpty()) {
+            val vert = stack.pop()
+
+            print("\t $vert")
+            for (i in 0 until vertCount) {
+                if (!visited[i]
+                    && matrix[vert][i] == 1
+                ) {
+                    stack.push(i)
+                    visited[i] = true
+                    count++
+                    if (count == vertCount) {
+                        return true
+                    }
+                }
+            }
+        }
+        println()
+
+        return false
+    }
+
 }
 
 //Input data
