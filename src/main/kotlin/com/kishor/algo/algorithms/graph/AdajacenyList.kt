cbf4e9d95b0aa3e78f33128974dd7ac92d334772
Kishor Sutar <kishor007sutar@gmail.com>
2020-07-08 21:46:55 +0100

    Added adjacency list weighted graph

--- src/main/kotlin/com/kishor/algo/algorithms/graph/AdajacenyList.kt
+++ src/main/kotlin/com/kishor/algo/algorithms/graph/AdajacenyList.kt
@@ -7,16 +7,16 @@ fun main() {
     adjL.accept(Scanner(System.`in`))
     adjL.display()
 }
-
-data class Edge(val src: Int, val dst: Int, val weight: Int) {
+private data class Edge(val src: Int, val dst: Int, val weight: Int) {
     override fun toString(): String {
         return "src: $src to dest: $dst weight: $weight"
     }
 }
 
+
 class AdajacenyList(vertCount: Int) {
 
-    var vertList = MutableList(vertCount) { mutableListOf<Edge>() }
+    private var vertList = MutableList(vertCount) { mutableListOf<Edge>() }
 
     fun accept(sc: Scanner) {
         println("Enter edge count: ")
