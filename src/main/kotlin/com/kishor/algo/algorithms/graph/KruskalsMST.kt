7f61a040f7c7186154066e8ac725dfd3488aa303
Kishor Sutar <kishor007sutar@gmail.com>
2020-07-15 23:03:06 +0100

    Added correct kruskals algorithm

--- src/main/kotlin/com/kishor/algo/algorithms/graph/KruskalsMST.kt
+++ src/main/kotlin/com/kishor/algo/algorithms/graph/KruskalsMST.kt
@@ -11,10 +11,10 @@ fun main() {
 //    println(unionFind.containsCycle())
 
     var sum = 0
-    val mst: List<Edge> = unionFind.kruskal()
+    val mst: Array<Edge?> = unionFind.kruskal()
     for (e in mst) {
         println("Edge $e")
-        sum += e.weight
+        sum += e!!.weight
     }
     println("\nTotal weight of MST = $sum")
     sc.close()
@@ -24,7 +24,7 @@ class KruskalsMST(val vertCount: Int) {
     var edgeCount: Int = 0
 
     private val mat = Array(vertCount) { Array(vertCount) { Int.MAX_VALUE } }
-    private var edgeList = mutableListOf<Edge>()
+    private lateinit var edgeList : Array<Edge?>
 
 
    /* private data class Edge(val src: Int, val dst: Int, val weight: Int) {
@@ -37,13 +37,14 @@ class KruskalsMST(val vertCount: Int) {
         println("Enter number of edges: ")
         edgeCount = sc.nextInt()
         println("Enter $edgeCount edges, src - dst - weight")
+        edgeList = arrayOfNulls(edgeCount)
         for (i in 0 until edgeCount) {
             val src = sc.nextInt()
             val dst = sc.nextInt()
             val wt = sc.nextInt()
             mat[src][dst] = wt
             mat[dst][src] = wt
-            edgeList.add(Edge(src, dst, wt))
+            edgeList[i] = Edge(src, dst, wt)
 //            edgeArray[i] = (Edge(src, dst, wt))
         }
     }
@@ -64,69 +65,55 @@ class KruskalsMST(val vertCount: Int) {
     }
 
     fun find(v: Int, parent: IntArray): Int {
-        var t = v
-        for (i in parent) {
-            if (i != -1) {
-                t = i
-            }
-        }
-        return t
+//        var t = v
+//        for (i in parent) {
+//            if (parent[i] != -1) {
+//                t = parent[i]
+//            }
+//        }
+//        return t
+
+        var v = v
+        while (parent[v] != -1) v = parent[v]
+        return v
     }
 
     fun union(src: Int, dst: Int, parent: IntArray) {
         parent[src] = dst
     }
 
-    private fun cotainsCycle(mst: List<Edge>, mstCount: Int): Boolean {
-        val parent = IntArray(vertCount) { -1 }
-
+    private fun cotainsCycle(mst: Array<Edge?>, mstCount: Int): Boolean {
+        val parent = IntArray(vertCount) {-1}
         for (i in 0 until mstCount) {
             val e = mst[i]
-            val sr = find(e.src, parent)
-            val dst = find(e.dst, parent)
-            if (sr == dst) {
-                return true
-            }
-            union(sr, dst, parent)
+            val sr = find(e!!.src, parent)
+            val dr = find(e.dst, parent)
+            if (sr == dr) return true
+            union(sr, dr, parent)
         }
-
-//        for (e in edgeArray) {
-//            val sr = find(e.src, parent)
-//            val dst = find(e.dst, parent)
-//            if (sr == dst) {
-//                return true
-//            }
-//            union(sr, dst, parent)
-//        }
-
         return false
     }
 
-    internal fun kruskal() : List<Edge> {
-        edgeList.sortWith(Comparator {e1, e2 -> e1.weight - e2.weight})
+    internal fun kruskal() : Array<Edge?> {
+        edgeList.sortWith(Comparator {e1, e2 -> e1!!.weight - e2!!.weight})
 //        edgeArray.sort(Comparator { o1: Edge, o2: Edge ->  o1.weight - o2.weight})
-        val mst = MutableList(vertCount-1) {Edge(0, 0, 0)}
-//        val mst = mutableListOf<Edge>()// {Edge(0, 0, 0)}
+//        val mst = MutableList(vertCount-1) {Edge(0, 0, 0)}
+        val mst = arrayOfNulls<Edge>(vertCount - 1)// {Edge(0, 0, 0)}
         var i = 0
         var mstEdgeCount = 0
 
 
         while (mstEdgeCount < vertCount - 1) {
-            if (i < edgeList.size) {
                 val e = edgeList[i]
                 i += 1
 
-//            mst.add(e)
                 mst[mstEdgeCount] = e
                 mstEdgeCount++
 
                 if (cotainsCycle(mst, mstEdgeCount)) {
                     mstEdgeCount -= 1
-                    mst[mstEdgeCount] = Edge(0, 0, 0)
+                    mst[mstEdgeCount] = null
                 }
-            } else {
-                break
-            }
         }
 
         return mst
