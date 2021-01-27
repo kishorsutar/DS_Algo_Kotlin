7bf0109618c987cc447e3f29d53e38c0f632f706
Kishor Sutar <kishor007sutar@gmail.com>
2021-01-27 20:45:53 +0000

    binary search updated

--- src/main/kotlin/com/kishor/algo/search/BinarySearch.kt
+++ src/main/kotlin/com/kishor/algo/search/BinarySearch.kt
@@ -1,8 +1,9 @@
 package com.kishor.algo.search
 
 fun main() {
-    val list = listOf<Int>(2, 4, 5, 6, 9, 12)
-    println("Binary Search ${BinarySearch().binarySearch(list, 1)}")
+//    val list = listOf<Int>(2, 4, 5, 6, 9, 12)
+    val list = arrayOf(2, 4, 5, 6, 9, 12)
+    println("Binary Search ${BinarySearch().binarySearch(list, 6)}")
 }
 
 class BinarySearch {
@@ -24,4 +25,25 @@ class BinarySearch {
 
         return -1
     }
+
+    fun binarySearch(list: Array<Int>,  item: Int): Int {
+        var left = 0
+        var right = list.size
+
+        while (left <= right) { // <= is imp since middle element might miss
+            var mid = (left + right)/2
+
+            if (list[mid] == item) {
+                return mid
+            }
+            if (item < list[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return -1
+    }
+
 }
