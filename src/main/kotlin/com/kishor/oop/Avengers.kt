979afc71615fbdf4565d1c199e4a0616e6829dc1
Kishor Sutar <kishor.sutar@tigerspike.com>
2021-02-18 11:36:14 +0000

    abstract classes in koltlin

---
+++ src/main/kotlin/com/kishor/oop/Avengers.kt
@@ -0,0 +1,46 @@
+package com.kishor.oop
+
+abstract class Avengers(val numbers: Int, val superPower: String) {
+
+    abstract fun assemble()
+    abstract fun protect()
+
+    fun attack() {
+        // some functionality
+    }
+
+}
+
+abstract class GalaxyWarriors(private val superPowers: String = "Galaxy") :
+    Avengers(superPower = superPowers, numbers = 4) {
+
+    abstract fun driveFast()
+
+    override fun assemble() {
+        println("on planet somewhere")
+    }
+
+    override fun protect() {
+        println("Protect command")
+    }
+}
+
+class Rocket : GalaxyWarriors() {
+    override fun driveFast() {
+        println("Drive the space ship")
+    }
+
+//    override fun protect() {
+//        println("Rocket command")
+//    }
+}
+
+class Hulk : Avengers(1, "Hulk smash") {
+    override fun assemble() {
+        println("Earth")
+    }
+
+    override fun protect() {
+        println("Always angry")
+    }
+}
