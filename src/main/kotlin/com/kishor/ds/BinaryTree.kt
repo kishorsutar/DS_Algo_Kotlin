4bb311c8c8439dccedb54d72b3ffbe9df9e37efa
Kishor Sutar <kishor007sutar@gmail.com>
2021-02-21 18:51:12 +0000

    Node's Depth sum probelm

--- src/main/kotlin/com/kishor/ds/BinaryTree.kt
+++ src/main/kotlin/com/kishor/ds/BinaryTree.kt
@@ -1,5 +1,7 @@
 package com.kishor.ds
 
+import java.util.*
+
 open class BinaryTree(value: Int) {
     var value = value
     var left: BinaryTree? = null
@@ -30,3 +32,74 @@ fun getBranchSum(node: BinaryTree, listOfSum: MutableList<Int>, runningSum: Int)
         getBranchSum(node.right!!, listOfSum, currentSum)
     }
 }
+
+fun nodeDepthsRecursive(root: BinaryTree): Int {
+    var runningSum = 0
+
+    return depthSum(root, runningSum)
+}
+
+fun depthSum(node: BinaryTree?, runningSum: Int): Int {
+
+    if (node == null) return 0
+
+    return runningSum + depthSum(node.right, runningSum) + depthSum(node.left, runningSum)
+
+}
+
+class Level(tree: BinaryTree, depth: Int) {
+    var depth = depth
+    var tree = tree
+}
+
+fun nodeDepthIterative(root: BinaryTree): Int {
+    val stack = Stack<Level>()
+    var sumOfDepth = 0
+
+
+    stack.push(Level(root, 0))
+
+    while (stack.isNotEmpty()) {
+
+        val top = stack.pop()
+        val node = top.tree
+        val depth = top.depth
+        sumOfDepth += depth
+        if (node.left != null) {
+            stack.push(Level(node.left!!, depth + 1))
+        }
+
+        if (node.right != null) {
+            stack.push(Level(node.right!!, depth + 1))
+        }
+    }
+
+    return sumOfDepth
+}
+
+
+fun nodeDepthLevelIterative(root: BinaryTree): Int {
+    val queue = ArrayDeque<Level>()
+    var sumOfDepth = 0
+
+
+    queue.add(Level(root, 0))
+
+    while (queue.isNotEmpty()) {
+
+        val top = queue.poll()
+        val node = top.tree
+        val depth = top.depth
+        sumOfDepth += depth
+        if (node.left != null) {
+            queue.add(Level(node.left!!, depth + 1))
+        }
+
+        if (node.right != null) {
+            queue.add(Level(node.right!!, depth + 1))
+        }
+    }
+
+    return sumOfDepth
+}
+
