542c67cd56c0a18e035d197776d7209495a38011
Kishor Sutar <kishor.sutar@tigerspike.com>
2021-02-22 12:05:01 +0000

    Creating anonymous interfaces in Kotlin

---
+++ src/main/kotlin/com/kishor/oop/AnonymousInterfaces.kt
@@ -0,0 +1,50 @@
+package com.kishor.oop
+
+fun main() {
+    val button = Button()
+    button.setListener(onClickListener = object : OnClickListener {
+        override fun onClick() {
+            println("button clicked setting anonymous")
+        }
+    })
+
+    button.click()
+
+    // or this
+    button.onClickListener = object : OnClickListener {
+        override fun onClick() {
+            println("button clicked anonymous")
+        }
+    }
+
+    button.click()
+
+    button.setListener(ViewClickHandler())
+
+    button.click()
+}
+
+class ViewClickHandler : OnClickListener {
+    override fun onClick() {
+        println("view clicked from class implementation")
+    }
+
+}
+
+interface OnClickListener {
+    fun onClick()
+}
+
+open class View {
+    lateinit var onClickListener: OnClickListener
+
+    fun setListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
+    }
+    fun click() {
+        onClickListener.onClick()
+    }
+}
+
+class Button: View()
+
